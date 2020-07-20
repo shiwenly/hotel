cb95d217547f79ee9a5ef6c8bf7a6cdaa6ad0acc
steven ly <shiwenly@gmail.com>
2020-07-20 20:39:23 +0200

    page room

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pages, only: [:new, :create]
+  get '/rooms', to: 'pages#rooms', as: :rooms
 end
